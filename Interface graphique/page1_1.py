fd62f6721f93a553141b1acd575b9fd7902cc8e9
johan_palos <johan.palos@ensg.eu>
2024-05-07 12:25:42 +0200

    Nouvelle version de l'interface graphique'

--- Interface graphique/page1_1.py
+++ Interface graphique/page1_1.py
@@ -14,9 +14,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(362, 271)
+        Form.resize(906, 750)
         self.groupBox = QtWidgets.QGroupBox(Form)
-        self.groupBox.setGeometry(QtCore.QRect(0, 0, 361, 271))
+        self.groupBox.setGeometry(QtCore.QRect(170, 120, 371, 281))
         self.groupBox.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.groupBox.setTitle("")
         self.groupBox.setObjectName("groupBox")
@@ -32,17 +32,9 @@ class Ui_Form(object):
         self.pushButton.setObjectName("pushButton")
         self.lineEdit = QtWidgets.QLineEdit(self.groupBox)
         self.lineEdit.setGeometry(QtCore.QRect(20, 140, 321, 51))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.lineEdit.setFont(font)
-        self.lineEdit.setStyleSheet("color: rgb(40, 83, 175);\n"
+        self.lineEdit.setStyleSheet("color: rgb(0, 0, 255);\n"
 "background-color: rgb(250, 207, 51);")
         self.lineEdit.setText("")
-        self.lineEdit.setFrame(False)
-        self.lineEdit.setCursorPosition(0)
-        self.lineEdit.setReadOnly(False)
         self.lineEdit.setObjectName("lineEdit")
         self.label = QtWidgets.QLabel(self.groupBox)
         self.label.setGeometry(QtCore.QRect(100, 80, 171, 51))
@@ -51,7 +43,7 @@ class Ui_Form(object):
         font.setBold(True)
         font.setWeight(75)
         self.label.setFont(font)
-        self.label.setStyleSheet("color: rgb(40, 83, 175);\n"
+        self.label.setStyleSheet("color :rgb(0, 0, 127)\n"
 "")
         self.label.setObjectName("label")
         self.frame = QtWidgets.QFrame(self.groupBox)
@@ -62,7 +54,7 @@ class Ui_Form(object):
         self.label_2 = QtWidgets.QLabel(self.frame)
         self.label_2.setGeometry(QtCore.QRect(0, 0, 311, 61))
         self.label_2.setText("")
-        self.label_2.setPixmap(QtGui.QPixmap("pokemon.png"))
+        self.label_2.setPixmap(QtGui.QPixmap("renommer.png"))
         self.label_2.setScaledContents(True)
         self.label_2.setObjectName("label_2")
 
@@ -70,22 +62,13 @@ class Ui_Form(object):
         QtCore.QMetaObject.connectSlotsByName(Form)
         
         self.pushButton.clicked.connect(Form.close)
-        
-        
-        self.user_input = ""
+
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.pushButton.setText(_translate("Form", "Jouer "))
         self.label.setText(_translate("Form", "Nouveau Joueur "))
-        
-    def on_play_clicked(self):
-        # Get the text from the QLineEdit and store it in the variable
-        self.user_input = self.lineEdit.text()
-        return self.user_input
-
-
 
 if __name__ == "__main__":
     import sys 
@@ -96,5 +79,3 @@ if __name__ == "__main__":
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
-    name_player = ui.on_play_clicked()
-    
